c5c57e5babee02524119d01c8a95a561490dd92e
fullstack_dev <dongying9159@outlook.com>
2025-05-23 16:32:10 -0600

    Update ecosystem configuration for PM2 and add PM2 script to package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       name: "haidoc-website",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
-      instances: "max",
+      instances: 1,
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
@@ -13,7 +13,7 @@ module.exports = {
       },
       autorestart: true,
       watch: false,
-      max_memory_restart: "1024M", // Restart if memory exceeds this
+      max_memory_restart: "2024M", // Restart if memory exceeds this
       error_file: "/var/log/pm2/nextjs-error.log",
       out_file: "/var/log/pm2/nextjs-out.log",
       log_date_format: "YYYY-MM-DD HH:mm Z",
